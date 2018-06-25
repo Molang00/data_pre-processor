6874fdc1109d876645101fb8b0bfab954b31b95a
daehuikim <40377750+daehuikim@users.noreply.github.com>
2018-06-25 10:06:43 +0900

    write_log
    write_log

---
+++ module/write_log/write_log.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import math
+
+class Write_log:
+    # gps좌표를 meter scale의 distance로 바꾸어준다
+    def measure_position(self, lat1, lon1, lat2, lon2):
+        R = 6378.137
+        dLat = (lat1 - lat2) * math.pi / 180
+        dLon = (lon1 - lon2) * math.pi / 180
+        a = math.sin(dLat / 2) * math.sin(dLat / 2) + math.cos(lat1 * math.pi / 180) * math.cos(
+            lat2 * math.pi / 180) * math.sin(dLon / 2) * math.sin(dLon / 2)
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        d = R * c
+        return d * 1000
+
+    def read_csv_files(self):
+        a =1
+
+    def 
