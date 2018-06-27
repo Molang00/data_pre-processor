bdf3c6985b3e429ae918d67bf2214a190d8fd61d
dmsghs <dmsghs@postech.ac.kr>
2018-06-27 11:13:12 +0900

    version1 complete
    remove och noise
    overspeed, out of field, over distance

--- module/controller/controller_module.py
+++ module/controller/controller_module.py
@@ -35,19 +35,15 @@ class Controller:
         :return:
         '''
 
-
-
-
-        object_extract = Extract_data()
         path_csv_folder = os.path.join(path_root_folder_input,name_folder).replace("\\", "/")
         path_output_folder_for_second_average = os.path.join(path_root_folder_output_for_min_average, name_folder).replace("\\", "/")
         path_output_folder_for_field = os.path.join(path_root_folder_output_for_field, name_folder).replace("\\", "/")
 
 
-        #object_extract.summarize_csv(path_csv_folder+"/", path_output_folder_for_second_average+"/")
+        object_extract = Extract_data()
+        object_extract.summarize_csv(path_csv_folder+"/", path_output_folder_for_second_average+"/")
 
         object_find_field = Find_field_csv()
-
         object_find_field.find_field_csv_folder(path_csv_folder+"/", path_field_info, path_output_folder_for_field+"/")
 
 
@@ -68,14 +64,13 @@ class Controller:
         print("FIND NOISE")
         print(path_root_folder_summarized_data + "\n", path_read_field_folder + "\n", path_root_folder_noise + "\n", name_folder)
         object_noise = NoiseFinder()
-        #object_noise.find_noise_in_data(path_root_folder_input, path_read_field_folder, path_root_folder_output1, name_folder)
+        object_noise.find_noise_in_data(path_root_folder_summarized_data, path_read_field_folder, path_root_folder_noise, name_folder)
 
         print("Edit Data")
         print(path_root_folder_to_cut + "\n",path_root_folder_noise+'\n' , path_root_folder_for_eddited_files + "\n", name_folder)
 
-        #2 30 5
         object_edit = EditData()
-        #object_edit.cut_error(path_root_folder_to_cut, path_root_folder_noise, path_root_folder_for_eddited_files, name_folder)
+        object_edit.cut_error(path_root_folder_to_cut, path_root_folder_noise, path_root_folder_for_eddited_files, name_folder)
 
 
 
