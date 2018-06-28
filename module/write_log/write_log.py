1f4b7d697eeb8d27a6951fe59fb3cfa5fa671e3d
daehuikim <40377750+daehuikim@users.noreply.github.com>
2018-06-28 18:12:21 +0900

    write_log
    write_log players배열까지만듬(excel에서있는 선수 상태표시줄 만든것)

--- module/write_log/write_log.py
+++ module/write_log/write_log.py
@@ -1,11 +1,7 @@
-#함수전반적으로 전달인자만 고치면된다.
-
-
-import pandas as pd
-import numpy as np
 import os
 import glob
 import math
+import re
 
 class Write_log:
 
@@ -63,12 +59,15 @@ class Write_log:
         # parsedfieldpath는 output.csv를 읽히는 것이다.
 
     def check_in_field(self, longitude, latitude, parsedFieldFilePath):
-        df = pd.read_csv(parsedFieldFilePath)
+        file_read=open(parsedFieldFilePath,'r',encoding="utf-8")
+        values = file_read.readlines()
         infield=False
-        for i in range(len(df)):
+        for i in range(len(values)):
+            data=values[i].split(',')
+            data[11]=data[11].replace("]",'0')
             # 경로,파일명,번호,구장이름,위도,경도
-            lonA, latA, lonB, latB, lonC, latC, lonD, latD = df.values[i][4:]
-
+            lonA, latA, lonB, latB, lonC, latC, lonD, latD = data[4:]
+            lonA, latA, lonB, latB, lonC, latC, lonD, latD =float(lonA),float(latA),float(lonB),float(latB),float(lonC),float(latC),float(lonD),float(latD)
             pointP = (longitude, latitude)
             pointA = (lonA, latA)
             pointB = (lonB, latB)
@@ -91,75 +90,80 @@ class Write_log:
 
     def check_activation(self,speed):
         statu=False
-        if speed>5 and speed<40:#속도평균이 5이상이면 플레잉중
+        if speed>3 and speed<40:#속도평균이 5이상이면 플레잉중
             statu=True
         return statu
 
     def detect_playing(self,path_csv_folder,path_field_info):
-        players=[[0]]
-        player=[]
-        starters=[]
-        bench=[]
-        statu_box=[3]
+        players=[]
         path_csv_folder=self.check_slash(path_csv_folder)
         files_csv = glob.glob(path_csv_folder + '*.csv')
         write_order = '//event,time,player_out,plater_in'
+        i=0
+        length=len(files_csv)
+        while(i<length):
+            files_csv[i]=files_csv[i].replace('\\','/')
+            i=i+1
 
         for file_csv in files_csv:
+            player = []
+            filename=file_csv.replace(path_csv_folder,'')
 
             file_to_read=open(file_csv,'r')
             read_values = file_to_read.readlines()
             data = read_values[1].split(',')
-            check_time_temp = data[0:5]
-            check_time_temp[3] = int(check_time_temp[3])  # hour
-            check_time_temp[4] = int(check_time_temp[4])  # minute
-            check_time_temp[5] = int(check_time_temp[5])  # second
+            check_time_temp = data[3:5]
+            check_time_temp[0] = int(check_time_temp[0])  # hour
+            check_time_temp[1] = int(check_time_temp[1])  # minute
             value_temp = data[6:9]
             value_temp[0]=float(value_temp[0]) #longitude
             value_temp[1] = float(value_temp[1]) #latitude
             value_temp[2] = float(value_temp[2]) #speed
+            longi=value_temp[0]
+            lati=value_temp[1]
             count = 1
-
             for read_value in read_values[2:]:
                 data = read_value.split(',')
-                check_time = data[0:6]
-                check_time[3] = int(check_time[3])  # hour
-                check_time[4] = int(check_time[4])  # minute
-                check_time[5] = int(check_time[5])  # second
+                check_time = data[3:5]
+                check_time[0] = int(check_time[0])  # hour
+                check_time[1] = int(check_time[1])  # minute
                 value = data[6:9]
                 value[0] = float(value[0])  # longitude
                 value[1] = float(value[1])  # latitude
                 value[2] = float(value[2])  # speed
 
-            if check_time==check_time_temp:
-                for i, v in enumerate(value):
-                    value_temp[i] = value_temp[i] + v
-                count = count + 1
-            else:
-                longi=value_temp[0]/count
-                lati=value_temp[1]/count
-                speed = value_temp[2]/count
-                playing = self.check_activation(speed)
-                infield = self.check_in_field(longi,lati,path_field_info)
-                if playing==True and infield==True:
-                    check=True
+                if check_time==check_time_temp:
+                    for i, v in enumerate(value):
+                        value_temp[i] = value_temp[i] + v
+                    count = count + 1
                 else:
-                    check=False
-                player.append([check_time_temp[3],check_time_temp[4],check])
-                #매 분마다 활동중인지 여부를 검사한 후 이를 배열로저장
-                check_time_temp=check_time
-                value_temp=value
-                count=1
-        players.append(player) #이거 indent위치맞나?
-        #이 위에까지 모든 csv파일을 읽어들여서 활동여부를 체크하는 형태로 저장
+                    longi=float(value_temp[0])/count
+                    lati=float(value_temp[1])/count
+                    speed = float(value_temp[2])/count
+                    playing = self.check_activation(speed)
+                    infield = self.check_in_field(longi,lati,path_field_info)
 
+                    if playing==True and infield==True:
+                        check=True
+                    else:
+                        check=False
+                    player.append([filename,check_time_temp[0],check_time_temp[1],check])
+                    #매 분마다 활동중인지 여부를 검사한 후 이를 배열로저장
+                    check_time_temp=check_time
+                    value_temp=value
+                    count=1
+            players.append(player)
+        # 이 위에까지 모든 csv파일을 읽어들여서 활동여부를 체크하는 형태로 저장
 
-        # 이 위치에 players안에 player안에 리스트 즉 3중리스트를 처리하되 시간정보를 저장하고 맨 앞시간과 맨 뒷시간정보확인
         # 그리고 여기서 sub시간찾아내야함
-        for u,p in enumerate(player):
-            player[u][0] #hour
-            player[u][1] #minute
+        for u,p in enumerate(players):
+            hour = players[u][0]  # hour
+            minute = players[u][1]  # minute
+            stamp_playing = players[u][2]  # 활동여부
 
+            if hour<min_hour and minute<min_minute:
+                min_hour=int(hour)
+                min_minute=int(minute)
 
 
         # log.csv적는부분(초기화부분고쳐야함)
@@ -191,14 +195,14 @@ class Write_log:
         file_to_write.close()
 
 
-if __name__ == "__main___":
-    csv_path = 'data/3. data_csv_second_average/'
-    field = 'data/2. data_csv_format/'
-    name_of_dir='드래곤즈0617'
 
-    Write_log()
-    writeObject = Write_log()
-    writeObject.detect_playing(csv_path+name_of_dir,field+name_of_dir+'/fieldtext.txt')
+csv_path = 'data/3. data_csv_second_average/'
+field = 'data/2. data_csv_format/'
+name_of_dir='드래곤즈 0617'
+
+Write_log()
+writeObject = Write_log()
+writeObject.detect_playing(csv_path+name_of_dir,field+name_of_dir+'/fieldmatch.txt')
 
 
 
