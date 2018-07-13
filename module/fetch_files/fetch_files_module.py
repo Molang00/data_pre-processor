2d5388f40d2149ce9e87cd3d8cc86456fed7b26a
dmsghs <dmsghs@postech.ac.kr>
2018-07-13 10:56:35 +0900

    make dynamic ftp download

--- module/fetch_files/fetch_files_module.py
+++ module/fetch_files/fetch_files_module.py
@@ -3,7 +3,7 @@ from model.fetch_files.ftp_model import Ftp_model
 
 class Fetch_files:
 
-    def download_recent_file(self, path_destination_folder = "data/0. data_gp_format", address_ftp_list = [("fitogether.co", 50021, "kleaguejunior2018", "junior2018")], access_date=0):
+    def download_recent_file(self, path_destination_folder_list = ["data/0. data_gp_format"], address_ftp_list = [("fitogether.co", 50021, "kleaguejunior2018", "junior2018")], access_date=0):
 
 
         object_ftp_model = Ftp_model()
@@ -11,6 +11,7 @@ class Fetch_files:
         for i in range(len(object_ftp_model.ftp_list)):
             ftp = object_ftp_model.ftp_list[i]
             category = address_ftp_list[i][3]
+            path_destination_folder = path_destination_folder_list[i]
             object_ftp_model.download_new_Files(ftp, access_date=access_date, category=category,
                                                 path_destination_folder=path_destination_folder)
 
